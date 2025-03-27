74c30bef25406f475bb48befd2ef83ee126bf701
Abdullah Javed <rajabdullah236@gmail.com>
2025-03-27 11:32:54 +0500

    Update Admin Panel.v9mna.js

--- src/pages/Admin Panel.v9mna.js
+++ src/pages/Admin Panel.v9mna.js
@@ -466,7 +466,7 @@ async function checkUserRole(user) {
 
         let repeaterData = $w("#repeater4").data;
 
-        $w("#repeater4").forEachItem(($item, itemData, index) => {
+        $w("#repeater4").onItemReady(($item, itemData, index) => {
           if (itemData.city && itemData.city.includes(filterValue)) {
             $item.show();
           } else {
